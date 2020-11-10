2025f6c09ccf1e13d93c380dffb3b94a2914b792
Aakash Jakhmola <aakash001jakhmola@gmail.com>
2020-11-10 21:28:00 +0530

    initial commit

---
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.services
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.tabs.TabLayout
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/services/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.services
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup)
+
+    }
+}
