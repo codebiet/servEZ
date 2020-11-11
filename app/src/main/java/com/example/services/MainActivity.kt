ee7ccf00290215544618f3f1a8c189bdffc2e387
Sumit Kumar Singh <sumitks866@gmail.com>
2020-11-11 11:36:00 +0530

    check login auth

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -12,7 +12,11 @@ import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.text.style.RelativeSizeSpan
 import android.text.style.StyleSpan
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,6 +27,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonLogin:Button = findViewById(R.id.button_login)
+        buttonLogin.setOnClickListener{
+            performLogin()
+        }
 
         val string:SpannableString = SpannableString("Don't have an account? Sign Up") ;
 
@@ -43,6 +52,16 @@ class MainActivity : AppCompatActivity() {
         textView6.setMovementMethod(LinkMovementMethod.getInstance()) ;
     }
 
+    private fun performLogin(){
+        val email = findViewById<EditText>(R.id.email_login).text
+        val password = findViewById<EditText>(R.id.password_login).text
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email.toString(),password.toString())
+            .addOnSuccessListener {
+                Log.d("Logs","$email\n$password\n${FirebaseAuth.getInstance().currentUser?.uid}")
+                email.clear()
+                password.clear()
+            }
+    }
 
 }
 
