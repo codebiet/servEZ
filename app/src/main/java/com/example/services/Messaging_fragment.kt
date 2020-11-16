20320924cc49186944839f1a432f4ffc8f11acb1
Sumit Kumar Singh <sumitks866@gmail.com>
2020-11-16 13:41:20 +0530

    minor changes

--- app/src/main/java/com/example/services/Messaging_fragment.kt
+++ app/src/main/java/com/example/services/Messaging_fragment.kt
@@ -6,19 +6,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.fragment_messaging.*
 
 class Messaging_fragment : Fragment() {
+
+    companion object {
+        fun newInstance() : Messaging_fragment = Messaging_fragment()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view : View = inflater.inflate(R.layout.fragment_messaging,container,false)
         return view ;
     }
 
+    val adaptor = GroupAdapter<ViewHolder>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d("Logs","Msg called")
+        recyclerview_latest_msg.adapter = adaptor
+        setDummy()
     }
 
-    companion object {
-        fun newInstance() : Messaging_fragment = Messaging_fragment()
+    private fun setDummy(){
+        adaptor.add(LatestMsg())
+        adaptor.add(LatestMsg())
+        adaptor.add(LatestMsg())
+        adaptor.add(LatestMsg())
+
     }
+
+    class LatestMsg(): Item<ViewHolder>(){
+        override fun bind(viewHolder: ViewHolder, position: Int) {
+
+        }
+
+        override fun getLayout(): Int {
+            return R.layout.latest_msg_tile
+        }
+    }
+
 }
--- app/src/main/java/com/example/services/messages/ChatActivity.kt
+++ app/src/main/java/com/example/services/messages/ChatActivity.kt
@@ -2,10 +2,17 @@ package com.example.services.messages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.services.R
+import com.example.services.models.User
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+        val myId = intent.getStringExtra("MY_ID")
+        val rcvId = intent.getStringArrayExtra("RCV_ID")
+
+
+
     }
 }
